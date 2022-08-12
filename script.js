c2087f59e0efd3ddbb66c247ec9a2b265121b328
oberonSilni <sinadamov@gmail.com>
2022-08-12 17:52:58 +0200

    winning combos added

--- script.js
+++ script.js
@@ -7,28 +7,49 @@ card.onclick = function(){
 
 
     if (counter%2 === 0 && card.dataset.pressed === "false"){
-
     let text1 = document.createElement("p");
     card.appendChild(text1);   
-    text1.textContent="x";
-     
+    text1.textContent="x";     
+    playerX.playerArray.push(card.dataset.key);
     counter++
 }
-
     else if(counter%2 > 0 && card.dataset.pressed === "false") {
     let text2 = document.createElement("p");
     card.appendChild(text2);   
     text2.textContent="o";
-
+    playerO.playerArray.push(Number(card.dataset.key));
     counter++
 }
 
 card.dataset.pressed = true;
-
-
-
-
+}
 }
 
 
+
+let playerFactory = (playerName) => {
+    playerArray = [];
+    playerScore = 0;
+return{
+    playerName,
+    playerArray
 }
+
+}
+
+let playerX = playerFactory("X");
+let playerO = playerFactory("O");
+
+
+
+let winningCombo = 
+[[1,2,3]
+[4,5,6]
+[7,8,9]
+[1,4,7]
+[2,5,8]
+[3,6,9]
+[1,5,9]
+[7,5,3]];
+
+
